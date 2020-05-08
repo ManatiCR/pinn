f8f8df6eda056270c834edaf5e45e7a9eaa7de2e
Edwin Torrez Barrera <edwtorba@yahoo.com>
2020-05-08 15:44:59 -0600

    - Apply new styles. - Add slider. - Add objectives. - Add pre-footer.

--- public/js/ctrl/default.js
+++ public/js/ctrl/default.js
@@ -2,8 +2,10 @@
 
 const accordions = require('../lib/accordions');
 const more = require('../lib/more');
+const sliders = require('../lib/sliders');
 
 module.exports = function () {
   accordions.init();
   more.init();
+  sliders.init();
 };
