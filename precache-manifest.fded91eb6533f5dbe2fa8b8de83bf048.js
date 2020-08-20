29ae993e0c777bd457ff32ebebe6ffdf7aab6c55
Edwin Torrez Barrera <edwtorba@yahoo.com>
2020-08-20 09:24:47 -0600

    - Build the app with ReactJS.

---
+++ precache-manifest.fded91eb6533f5dbe2fa8b8de83bf048.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "192f1ee1516ec014392798615fb08e3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70c074d305c0828a1725",
+    "url": "/static/css/2.1a8daee9.chunk.css"
+  },
+  {
+    "revision": "a9f40da3576dcc68a18f",
+    "url": "/static/css/main.4327cba6.chunk.css"
+  },
+  {
+    "revision": "70c074d305c0828a1725",
+    "url": "/static/js/2.63c1ae42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.63c1ae42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9f40da3576dcc68a18f",
+    "url": "/static/js/main.0f623352.chunk.js"
+  },
+  {
+    "revision": "6968402b28c70a06da11",
+    "url": "/static/js/runtime-main.cf4eeb1a.js"
+  },
+  {
+    "revision": "6b4f8b00110ff623b813ee33fe1ce417",
+    "url": "/static/media/anexo-1-hoja-de-vida-bootcamps.6b4f8b00.docx"
+  },
+  {
+    "revision": "ede29f65f8bd1227c529472a97f1560b",
+    "url": "/static/media/anexo-2-declaracion-jurada-bootcamps.ede29f65.docx"
+  },
+  {
+    "revision": "6a0cce4a9e1d0989e8e67d6faf1108ed",
+    "url": "/static/media/anexo-3-consentimiento-informado-bootcamps.6a0cce4a.docx"
+  },
+  {
+    "revision": "dd9fe5cdf2ab63ec6cc21f8a72db836f",
+    "url": "/static/media/banner-aumentar-productividad-1.dd9fe5cd.jpg"
+  },
+  {
+    "revision": "802491225cdf07f113d8f267dffd7063",
+    "url": "/static/media/banner-aumentar-productividad-2.80249122.jpg"
+  },
+  {
+    "revision": "7a104cb4207e749263c4a313d8e38f7f",
+    "url": "/static/media/banner-capacitaciones.7a104cb4.jpg"
+  },
+  {
+    "revision": "5f49ba845fce04728d7b2d07b8341dd2",
+    "url": "/static/media/banner-certificaciones.5f49ba84.jpg"
+  },
+  {
+    "revision": "01c1a4e2853398e2bb7524e6ccd80078",
+    "url": "/static/media/banner-mercado-laboral.01c1a4e2.jpg"
+  },
+  {
+    "revision": "77e89b5d0fbd5213efc700c0a33cf186",
+    "url": "/static/media/logo.77e89b5d.png"
+  },
+  {
+    "revision": "38224218ba20c18cad4b3efbbfab709e",
+    "url": "/static/media/obj-competitiveness-and-innovation.38224218.svg"
+  },
+  {
+    "revision": "f5c98ba0147111283daf01bc45ba9f23",
+    "url": "/static/media/obj-human-capital.f5c98ba0.svg"
+  },
+  {
+    "revision": "a4f0bd6e26788c271a2b280c843a0c91",
+    "url": "/static/media/obj-increase-productivity.a4f0bd6e.svg"
+  },
+  {
+    "revision": "de65c69ff91bbaff59c63e749932afbd",
+    "url": "/static/media/obj-professional-qualification.de65c69f.svg"
+  },
+  {
+    "revision": "e324e7245812f38a1183981b5131c44d",
+    "url": "/static/media/obj-support-capabilities.e324e724.svg"
+  },
+  {
+    "revision": "a311f563cf6307813c06edd339327be6",
+    "url": "/static/media/obj-trainings.a311f563.svg"
+  },
+  {
+    "revision": "40c3c69e2d7bf72361a12721d8bfbd73",
+    "url": "/static/media/provider-bootcamp.40c3c69e.svg"
+  },
+  {
+    "revision": "ec2c64d26da6c3ff6269bac6434b1c4c",
+    "url": "/static/media/provider-international.ec2c64d2.svg"
+  },
+  {
+    "revision": "1fac40d2a113fe3a2958a7981894f6cf",
+    "url": "/static/media/provider-national.1fac40d2.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
