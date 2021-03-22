f5543948f5c9df4c6c4747ccd30b746396913bf5
Edwin Torrez Barrera <edwtorba@yahoo.com>
2021-03-22 14:11:47 -0600

    Updates

--- react-app/build/service-worker.js
+++ react-app/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pinn/precache-manifest.a385d23b930f22eeb9d28b5d45f54168.js"
+  "/precache-manifest.332aeb3c387c693b3fc35cf971d82a06.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pinn/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
