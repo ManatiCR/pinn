06c253238c0dcebccf7325cc888f5a1e2f8c4909
Edwin Torrez Barrera <edwtorba@yahoo.com>
2021-04-16 18:56:12 -0600

    - Close the calls.

--- src/components/Oportunities/index.jsx
+++ src/components/Oportunities/index.jsx
@@ -113,7 +113,7 @@ function Oportunities({ apiUrl }) {
             ]}
             items={bootCampsData}
             key="accordion-bootcamps"
-            applyLink="http://eepurl.com/htDZ7H"
+            applyLink=""
             closeText="Convocatoria cerrada"
           />
           <Accordion
@@ -138,7 +138,7 @@ function Oportunities({ apiUrl }) {
             ]}
             items={internationalsData}
             key="accordion-internationals"
-            applyLink="http://eepurl.com/htFy5b"
+            applyLink=""
             closeText="Convocatoria cerrada"
           />
           <Accordion
